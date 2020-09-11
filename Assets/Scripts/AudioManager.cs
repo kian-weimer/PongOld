7b634642874354d05abe575a8d92998911a851f0
Kian Weimer <kianweimer@gmail.com>
2020-09-11 11:33:32 -0500

    Created initial game.
    Inital pong game created. Paddle and Ball assets made. Paddles can move up and down. Ball can not yet move.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Awake ()
+    {
+        foreach (Sound s in sounds)
+        {
+            gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
